ab5b66e18b7e57e8a3165cfb0cb83f6f976120ce
LoisNgm <ngmhuiminlois@gmail.com>
2021-05-07 12:11:14 +0800

    add missing files

---
+++ tt4-app/src/components_login/Login.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="login-wrapper">
+            <h1>Please Log In</h1>
+            <form>
+            <label>
+                <p>Username</p>
+                <input type="text" />
+            </label>
+            <label>
+                <p>Password</p>
+                <input type="password" />
+            </label>
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+            </form>
+        </div>
+    )
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
+
+export default Login
