5d3eec4ef28164f25879b6a4c013e5bdc15be33c
LoisNgm <ngmhuiminlois@gmail.com>
2021-05-07 12:26:27 +0800

    login success

--- tt4-app/src/components_login/Login.js
+++ tt4-app/src/components_login/Login.js
@@ -1,18 +1,41 @@
-import React from 'react'
-import './Login.css'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
-function Login() {
+import './Login.css'
+async function loginUser(credentials) {
+    return fetch('https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(data => data.json())
+   }
+
+function Login({ setToken }) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+          username,
+          password
+        });
+        setToken(token);
+      }
     return (
         <div className="login-wrapper">
             <h1>Please Log In</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
             <label>
                 <p>Username</p>
-                <input type="text" />
+                <input type="text" onChange={e => setUserName(e.target.value)}/>
             </label>
             <label>
                 <p>Password</p>
-                <input type="password" />
+                <input type="password" onChange={e => setPassword(e.target.value)}/>
             </label>
             <div>
                 <button type="submit">Submit</button>
@@ -20,6 +43,10 @@ function Login() {
             </form>
         </div>
     )
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
 }
 
 export default Login
